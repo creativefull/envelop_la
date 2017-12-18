a36a930c034ac7f15c4f863c2614853792f7a237
Shodiqul Muzaki <user.creativefull@gmail.com>
2017-12-18 15:43:27 +0700

    change first name last name

--- updated_content.php
+++ updated_content.php
@@ -28,7 +28,8 @@
 
         public function get($step) {
             global $wpdb;
-            $userid = wp_get_current_user()->id;
+            $userInfo = wp_get_current_user();
+            $userid = $userInfo->id;
             $query = $wpdb->get_results("SELECT * FROM tbl_content_env WHERE step='$step' AND userid='" . $userid . "'");
             if (count($query) > 0) {
                 return $query[0]->content;
@@ -52,6 +53,8 @@ Can we have an area where the student puts:
         public function getTemplate($step) {
             global $wpdb;
             $userinfo = wp_get_current_user();
+            $name = $userinfo->user_firstname . " " . $userInfo->user_lastname;
+
             $strx = "SELECT M.*, C.content FROM tbl_env_market as M INNER JOIN tbl_content_env AS C ON (C.userid = M.userid && C.step = M.seq) WHERE M.userid='$userinfo->id' AND M.seq=$step";
             $query = $wpdb->get_results($strx);
             $content = array();
@@ -79,7 +82,7 @@ Can we have an area where the student puts:
                         $q->state,
                         $q->city,
                         $q->zipcode,
-                        $userinfo->display_name
+                        $name
                     );
                     $content[] = str_replace($searchFormat, $replaceFormat, $q->content);
                 }
